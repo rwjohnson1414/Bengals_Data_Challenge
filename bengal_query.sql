24943bb4b0adad22f8649181481d8d614b939807
rjohnson387 <rjohnson387@gatech.edu>
2022-10-12 11:44:30 -0400

    slight adjustment to SQL format

--- bengal_query.sql
+++ bengal_query.sql
@@ -1,6 +1,6 @@
-SELECT SUM("boyd_Yards") as "Boyd Total Yards", 
-	   SUM("chase_Yards") as "Chase Total Yards",
-	   SUM("higgins_Yards") as "Higgins Total Yards",
-	   concat(Count("Result") filter (where "Result" = 'Win'), '-', 
-	   		  Count("Result") filter (where "Result" = 'Loss')) as "Win/Loss" 
+SELECT sum("boyd_Yards") as "Boyd Total Yards", 
+       sum("chase_Yards") as "Chase Total Yards",
+       sum("higgins_Yards") as "Higgins Total Yards",
+       concat(count("Result") filter (where "Result" = 'Win'), '-', 
+	          count("Result") filter (where "Result" = 'Loss')) as "Win/Loss" 
 FROM public.reese_johnson
